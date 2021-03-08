07b6375444ef6847a6e7de283afa4baf66ac9862
unique218 <gggks888@gmail.com>
2021-03-08 16:06:15 +0900

    added output - bitKeeper

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git!!!");
 		System.out.println("What is Git?");
-		System.out.println("That's DVCS");=======
+		System.out.println("That's DVCS");
 		System.out.println("Linux kernel used BitKeeper DVCS");
 
 
